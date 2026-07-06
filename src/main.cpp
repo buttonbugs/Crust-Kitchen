7b6a2308d32309bb6b213b3930b3523214eb64ab
buttonbugs <114892917+buttonbugs@users.noreply.github.com>
2026-07-05 19:40:27 -0700

    LED blinking test

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,15 @@
 #include <Arduino.h>
-#include <FastLED.h>
-
-#define NUM_LEDS 144         // The number of LEDs on one LED strip
-#define LED_STRIP_DATA 10
-#define LED_STRIP_SCK 8
-
-#define COLOR_ORDER BGR     // Check physical channel order. BGR is common for SK9822; APA102 may vary
-
-CRGB leds[NUM_LEDS];
-int color_index = 0;
-CRGB color[] = {CRGB::White, CRGB::Red, CRGB::Green, CRGB::Blue};
+// #include <FastLED.h>
 
 void setup() {
+    Serial.begin(115200);
     pinMode(10, OUTPUT);
-    Serial.begin(9600);
-    FastLED.addLeds<SK9822, LED_STRIP_DATA, LED_STRIP_SCK, COLOR_ORDER>(leds, NUM_LEDS);
-    FastLED.setBrightness(50);  // Set initial brightness (0-255)
-    delay(5000);
 }
 
 void loop() {
-    for (int i = 0; i < NUM_LEDS; i++) {
-        for (int j = 0; j < NUM_LEDS; j++) {
-            leds[j] = i==j ? color[color_index] : CRGB:: Black;
-        }
-        Serial.println(i);
-        FastLED.show();
-        delay(10);
-    }
-    digitalWrite(10, color_index % 2);
-    color_index = (color_index + 1) % 4;
+    Serial.println("Ha");
+    digitalWrite(10, HIGH);
+    delay(1000);
+    digitalWrite(10, LOW);
+    delay(1000);
 }
